65dd9f8d4b589de8e0134c2a51766d6663f6e81a
blancaaz <blancaaz@student.21-school.ru>
2025-10-11 16:24:03 +0300

    version 2 is done

---
+++ ex1_6/2/main.go
@@ -0,0 +1,31 @@
+package main
+//gorutine закрывается через контекст
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				ch <- struct{}{}
+				return
+			default:
+				fmt.Println("ping")
+			}
+
+			time.Sleep(500 * time.Millisecond)
+		}
+	}(ctx)
+	go func() {
+		time.Sleep(3 * time.Second)
+		cancel()
+	}()
+	<-ch
+	fmt.Println("the end!")
+}
